53ab35681b6a2524ea5c817e3762f25b7a09539e
Constipated <ziertemha@gmail.com>
2021-12-22 20:45:32 -0600

    added toggle button functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 
+
 function App() {
+
+  const [dark, setDark] = useState(false);
+
+  function changeMode() {
+    setDark(!dark);
+  }
+
   return (
     <div className="App">
-    <Navbar />
+    <Navbar dark={dark} changeMode={changeMode}/>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,21 @@ import SunIcon from "../../assets/desktop/icon-sun.svg";
 import MoonIcon from "../../assets/desktop/icon-moon.svg";
 import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+
     return (
         <nav className="nav-bar">
             <h1>devjobs</h1>
             <img src={SunIcon} />
+            <div onClick={() => {
+                props.changeMode();
+                console.log(props.dark);
+            }} className="toggle-btn">
+                <div style={props.dark === true ? {marginLeft: "auto"} : null} className="inner-circle">
+
+                </div>
+            </div>
             <img src={MoonIcon} />
         </nav>
     )
